6fa8bff8d044a4b11a1ccb3d7178606cfee7e352
agrosu <alexandrudoru.grosu+github@gmail.com>
2015-09-19 18:01:06 +0200

    Event handler for alarm.

--- ReactorSOCKFacade/Event_Log_Demux.cpp
+++ ReactorSOCKFacade/Event_Log_Demux.cpp
@@ -17,15 +17,19 @@ void Event_Log_Demux::handle_log(char in[], int len)
 	char log_type = in[0];
 	switch (log_type)
 	{
-		case '1': handle_alarm(std::string(in, 2, len));
-		case '2': handle_patient_info(std::string(in, 2, len));
-		case '3': handle_simple_log(std::string(in, 2, len));
+		case '1': handle_alarm(std::string(in, 2, len)); break;
+		case '2': handle_patient_info(std::string(in, 2, len)); break;
+		case '3': handle_simple_log(std::string(in, 2, len)); break;
 	}
 }   
 
 void Event_Log_Demux::handle_alarm(std::string alarm)
 {
-	std::cout << "Event_Log_Demux->got alarm=" << alarm << std::endl;
+	typedef std::string::size_type str_size;
+	str_size priority_end = alarm.find("|");
+	str_size priority_text = alarm.find("|", priority_end + 1);
+	std::cout << "Alarm priority: " << alarm.substr(0, priority_end) << std::endl;
+	std::cout << "Alarm text: " << alarm.substr(priority_end + 1, priority_text) << std::endl;
 }
 
 void Event_Log_Demux::handle_patient_info(std::string patient_info)
