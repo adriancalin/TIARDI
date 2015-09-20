b4116ab32796b53c4900a45817003b8f082e1a4b
agrosu <alexandrudoru.grosu+github@gmail.com>
2015-09-20 12:10:38 +0200

    Complete handlers for Event_Log_Demux

--- ReactorSOCKFacade/Event_Log_Demux.cpp
+++ ReactorSOCKFacade/Event_Log_Demux.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "Event_Log_Demux.h"
 
+typedef std::string::size_type str_size;
 
 Event_Log_Demux::Event_Log_Demux()
 {
@@ -14,9 +15,11 @@ Event_Log_Demux::~Event_Log_Demux()
 
 void Event_Log_Demux::handle_log(char in[], int len)
 {
+	// Delegate based on event type
 	char log_type = in[0];
 	switch (log_type)
 	{
+		// Convert buffer to std::string for easier manipulation
 		case '1': handle_alarm(std::string(in, 2, len)); break;
 		case '2': handle_patient_info(std::string(in, 2, len)); break;
 		case '3': handle_simple_log(std::string(in, 2, len)); break;
@@ -25,7 +28,6 @@ void Event_Log_Demux::handle_log(char in[], int len)
 
 void Event_Log_Demux::handle_alarm(std::string alarm)
 {
-	typedef std::string::size_type str_size;
 	str_size priority_end = alarm.find("|");
 	str_size priority_text = alarm.find("|", priority_end + 1);
 	std::cout << "Alarm priority: " << alarm.substr(0, priority_end) << std::endl;
@@ -34,10 +36,13 @@ void Event_Log_Demux::handle_alarm(std::string alarm)
 
 void Event_Log_Demux::handle_patient_info(std::string patient_info)
 {
-	std::cout << "Event_Log_Demux->got patient_info=" << patient_info << std::endl;
+	str_size type_end = patient_info.find("|");
+	str_size value_end = patient_info.find("|", type_end + 1);
+	std::cout << "Patient event type: " << patient_info.substr(0, type_end) << std::endl;
+	std::cout << "Patient event value: " << patient_info.substr(type_end + 1, value_end) << std::endl;
 }
 
 void Event_Log_Demux::handle_simple_log(std::string simple_log)
 {
-	std::cout << "Event_Log_Demux->got simple_log=" << simple_log << std::endl;
+	std::cout << "Log text: " << simple_log << std::endl;
 }
